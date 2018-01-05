ce16f99db57f089a8ccf97e0656cd2884a5d5a61
Dendi Sunandar <dendi@mkitech.co.id>
2018-01-05 15:12:50 +0700

    tambah CRUT, tapi ko gabisa jalanin laravue di vhost?

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,13 +8,25 @@ import axios from 'axios';
 Vue.use(VueAxios, axios);
 
 import App from './App.vue';
-import Example from './components/ExampleComponent.vue';
+import CreateItem from './components/CreateItem.vue';
+import DisplayItem from './components/DisplayItem.vue';
+import EditItem from './components/EditItem.vue';
 
 const routes = [
     {
-        name: 'Example',
+        name: 'CreateItem',
+        path: '/items/create',
+        component: CreateItem
+    },
+    {
+        name: 'DisplayItem',
         path: '/',
-        component: Example
+        component: DisplayItem
+    },
+    {
+        name: 'EditItem',
+        path: '/edit/:id',
+        component: EditItem
     }
 ];
 
